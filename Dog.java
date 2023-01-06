ea12937edb7bcadc62bb53d2cb3101bf402c9830
Jeah Mae Agustin <116334093+jeahagustin@users.noreply.github.com>
2023-01-06 23:34:28 +0800

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,18 @@
+public class Dog extends Pet
+{
+	private String breed;
+	public Dog(String name, double weight, String breed){
+		super(name, weight);
+		this.breed = breed;
+	}
+	@Override
+	public String makeSound(){
+		return "Arf! Arf!";
+	}
+	public String getBreed(){
+		return breed;
+	}
+	public void setBreed(String breed){
+		this.breed = breed;
+	}
+}
